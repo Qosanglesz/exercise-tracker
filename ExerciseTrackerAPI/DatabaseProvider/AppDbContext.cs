98836b26593cdd2af696352db14790408a34fcfc
Wissarut Kanasub <wissarut.dev@gmail.com>
2025-06-05 13:59:54 +0700

    Create docker-compose.yml and connect to database

--- ExerciseTrackerAPI/DatabaseProvider/AppDbContext.cs
+++ ExerciseTrackerAPI/DatabaseProvider/AppDbContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ExerciseTrackerAPI.DatabaseProvider;
 
-public class AppContext: DbContext {
-    public AppContext(DbContextOptions<AppContext> option) : base(option) {}
+public class AppDbContext: DbContext {
+    public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) {}
     public DbSet<Task> Tasks { get; set; }
 }
--- ExerciseTrackerAPI/Program.cs
+++ ExerciseTrackerAPI/Program.cs
@@ -1,6 +1,17 @@
+using ExerciseTrackerAPI.DatabaseProvider;
+using Microsoft.EntityFrameworkCore;
+
+// App's builder Zone
 var builder = WebApplication.CreateBuilder(args);
+
+// App Service registration zone
+
+// Register Database to AppDbContext
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetSection("database:ConnectionStrings")["docker-compose"]));
+
+// App Zone
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
